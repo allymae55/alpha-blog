3c3e3ec21f4b16fe113fc1a2a33ad469e7fda722
Allymae <alice.neville@okstate.edu>
2021-07-23 15:25:04 -0500

    add new article to DBm

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,12 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article]
-
+        #render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        #render plain: @article.inspect
+        #redirect_to  article_path(@article)
+        #shortcut to above line ^
+        redirect_to  @article
     end
 end
