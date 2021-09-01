60866eb4acd4ea63e1a14f32488787372066be77
Allymae <alice.neville@okstate.edu>
2021-09-01 14:14:16 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+	validates :username, presence: true, 
+						uniqueness: { case_sensitive: false }, 
+						length: {minimum: 3, maximum: 25}
+	VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+	validates :email, presence: true,  
+						uniqueness: { case_sensitive: false }, 
+						length: {maximum: 105},
+						format: {with: VALIDATE_EMAIL_REGEX }
 end
