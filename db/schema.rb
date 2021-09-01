b235664334ac58458a0fd16bf679fd7fe71bf9e6
Allymae <alice.neville@okstate.edu>
2021-09-01 16:39:36 -0500

    Add Secure Password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_194515) do
+ActiveRecord::Schema.define(version: 2021_09_01_212831) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_194515) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
