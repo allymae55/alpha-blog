4f5f8b5c9915840b0424e308bbcb9e52488ec4ca
Allymae <alice.neville@okstate.edu>
2021-09-02 13:44:32 -0500

    users-signup frontend

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
