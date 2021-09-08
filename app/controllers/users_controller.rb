24231fa6bfff2ab8c838ab7efccb3c79c8edaa25
Allymae <alice.neville@okstate.edu>
2021-09-08 13:03:59 -0500

    Require user and same user for users edit 166

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update] 
 
   def show
   	  	@articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -32,12 +34,17 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
-private
-def user_params
-	params.require(:user).permit(:username, :email, :password)
-end
-def set_user
-  @user = User.find(params[:id])
-end
-
+  private
+  def user_params
+  	params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def require_same_user
+      if current_user != @user
+          flash[:alert] = "You can only edit your own profile"
+          redirect_to @user
+      end
+  end
 end
